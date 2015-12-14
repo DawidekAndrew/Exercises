0358249416a8fb58c7245b455011ad8838f6c633
Andrew Dawidek <dawidekandy@hotmail.com>
2015-12-13 22:58:53 -0500

    Updated notes for problem 5

--- Project Euler/Q5.cpp
+++ Project Euler/Q5.cpp
@@ -13,13 +13,13 @@ Consider the numbers 1 to 8
 1 is prime so multiply by 1 (if you really feel like it)
 2 is prime so multiply by 2
 3 is prime so multiply by 3
-4 is not prime. It can be broken down into 2 * 2. Since the product is divisible by 2, only multiplied by 2
+4 is not prime. It can be broken down into 2 * 2. Since the product is divisible by 2, only multiply by 2
 5 is prime so multiply by 5
 6 is not prime. It can be broken down into 2 * 3, both of which have already been acounted for so no multiplication
 7 is prime so multiply by 7
 8 is not prime. It can be broken down into 2 * 4. Since the product is now divisible by 4, only multiply by 2.
 
-The trick to the algorithm is that whenever a new non-prime number x is found that is not already a factor of the current product,
+The trick to the algorithm is that whenever a new non-prime number x is found (that is not already a factor of the current product),
 it is guaranteed that the product only needs to be multiplied by a SINGLE prime factor of x to make the product now divisible 
 by x.
 
